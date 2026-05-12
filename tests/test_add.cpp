95968414bf1be957171ddc5fbfb0215230a181db
Sina Eghbal <eghbalsa@mail.uc.edu>
2026-05-12 21:16:33 +1000

    Using statements removed.

--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -3,20 +3,15 @@
 #include "solutions.h"
 #include "helper/helper.h"
 
-using std::vector;
-using std::random_device;
-using std::mt19937;
-using std::uniform_int_distribution;
-
 TEST(MathTest, Addition) {
     const int n_test = 1000;
-    random_device rd;
-    mt19937 generator(rd());
-    uniform_int_distribution<> num_dist(INT_MIN, INT_MAX);
+    std::random_device rd;
+    std::mt19937 generator(rd());
+    std::uniform_int_distribution<> num_dist(INT_MIN, INT_MAX);
 
     for (int i = 0; i < n_test; i++) {
         const int a = num_dist(generator);
         const int b = num_dist(generator);
-        test_function(add, vector<int>({a + b}), a, b);
+        test_function(add, std::vector<int>({a + b}), a, b);
     }
 }
